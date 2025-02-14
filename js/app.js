21c15023a56434949857ff211a82ee1f3c53e4c1
Asafe Roger <134728635+AsafeRoger@users.noreply.github.com>
2025-02-14 00:35:05 -0300

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,21 @@
+
+function includeHTML(elementId, file) {
+    fetch(file)
+        .then(response => response.text())
+        .then(data => document.getElementById(elementId).innerHTML = data)
+        .catch(error => console.log("Erro ao carregar " + file + ": " + error));
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    // Incluir o menu lateral e a navbar
+    includeHTML("container-sidebar", "sidebar.html");
+    includeHTML("container-navbar", "navbar.html");
+    
+});
+
+function toggleMenu(element){
+    
+    document.getElementById("sidebar").classList.toggle("show");
+}
+
